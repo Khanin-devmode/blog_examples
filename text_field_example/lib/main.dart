fa522210f0b1b7e290c4bceb49d896cb23443b38
Khanin Chomphusri <nin.khanin@oivan.com>
2023-11-07 16:10:15 +0700

    widgetbook app

--- text_field_example/lib/main.dart
+++ text_field_example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: ''),
     );
   }
 }
@@ -38,71 +38,71 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   title: Text(widget.title),
+      // ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text(
-                'Max Lines 5',
-              ),
+              // const Text(
+              //   'Max Lines 5',
+              // ),
+              // TextFormField(
+              //   decoration: InputDecoration(border: outlineInputBorder),
+              //   maxLines: 4,
+              // ),
+              // const SizedBox(height: 16.0),
               TextFormField(
                 decoration: InputDecoration(border: outlineInputBorder),
-                maxLines: 4,
+                minLines: 2,
+                maxLines: 1,
               ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(border: outlineInputBorder),
-                minLines: 1,
-                maxLines: 4,
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(border: outlineInputBorder),
-              ),
-              const SizedBox(height: 16.0),
-              Row(
-                children: [
-                  Expanded(
-                      child: Container(
-                    height: 100,
-                    color: Colors.amber,
-                  )),
-                  Expanded(
-                    child: TextFormField(
-                      style: TextStyle(fontSize: 60, height: 1),
-                      decoration: InputDecoration(
-                          border: outlineInputBorder,
-                          contentPadding: const EdgeInsets.all(20)),
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(height: 16),
-              Row(
-                children: [
-                  Expanded(
-                      child: Container(
-                    height: 100,
-                    color: Colors.amber,
-                  )),
-                  Expanded(
-                    child: TextFormField(
-                      style: TextStyle(fontSize: 20, height: 1),
-                      decoration: InputDecoration(
-                        border: outlineInputBorder,
-                        contentPadding: const EdgeInsets.all(20),
-                      ),
-                      maxLines: 3,
-                    ),
-                  )
-                ],
-              )
+              // const SizedBox(height: 16.0),
+              // TextFormField(
+              //   decoration: InputDecoration(border: outlineInputBorder),
+              // ),
+              // const SizedBox(height: 16.0),
+              // Row(
+              //   children: [
+              //     Expanded(
+              //         child: Container(
+              //       height: 100,
+              //       color: Colors.amber,
+              //     )),
+              //     Expanded(
+              //       child: TextFormField(
+              //         style: TextStyle(fontSize: 60, height: 1),
+              //         decoration: InputDecoration(
+              //             border: outlineInputBorder,
+              //             contentPadding: const EdgeInsets.all(20)),
+              //       ),
+              //     )
+              //   ],
+              // ),
+              // const SizedBox(height: 16),
+              // Row(
+              //   children: [
+              //     Expanded(
+              //         child: Container(
+              //       height: 100,
+              //       color: Colors.amber,
+              //     )),
+              //     Expanded(
+              //       child: TextFormField(
+              //         style: TextStyle(fontSize: 20, height: 1),
+              //         maxLines: 3,
+              //         decoration: InputDecoration(
+              //           border: outlineInputBorder,
+              //           contentPadding: const EdgeInsets.all(20),
+              //         ),
+              //       ),
+              //     ),
+              //   ],
+              // )
             ],
           ),
         ),
---
+++ widgetbook_example/android/app/src/main/kotlin/com/example/widgetbook_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.widgetbook_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
