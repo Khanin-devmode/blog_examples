8262d8d0ec7883c34a91ccf23b3e76aecdec721e
Khanin Chomphusri <nin.khanin@oivan.com>
2024-02-06 15:14:53 +0700

    init ticket_widget_example

--- widgetbook_example/lib/main.dart
+++ widgetbook_example/lib/main.dart
@@ -10,74 +10,90 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Widgetbook.material(directories: [
-      WidgetbookFolder(
-        name: 'Atoms',
-        children: [
-          WidgetbookComponent(
-            name: 'Component1',
-            useCases: [
-              WidgetbookUseCase(
-                name: 'Usecase1',
-                builder: (BuildContext context) {
-                  return Scaffold(
-                    body: Center(
-                      child: Container(
-                        color: Colors.amberAccent,
-                        width: context.knobs.double.slider(
-                            label: 'width',
-                            min: 10,
-                            max: 100,
-                            initialValue: 50),
-                        height: context.knobs.double.slider(
-                            label: 'height',
-                            min: 10,
-                            max: 100,
-                            initialValue: 50),
-                      ),
-                    ),
-                  );
-                },
-              ),
-              WidgetbookUseCase(
-                name: 'Usecase2',
-                builder: (BuildContext context) {
-                  return Scaffold();
-                },
-              ),
-              WidgetbookUseCase(
-                name: 'Usecase3',
-                builder: (BuildContext context) {
-                  return Scaffold();
-                },
-              ),
-            ],
-          ),
-          WidgetbookComponent(
-            name: 'Component2',
-            useCases: [
-              WidgetbookUseCase(
-                name: 'Usecase1',
-                builder: (BuildContext context) {
-                  return Scaffold();
-                },
-              ),
-              WidgetbookUseCase(
-                name: 'Usecase2',
-                builder: (BuildContext context) {
-                  return Scaffold();
-                },
-              ),
-              WidgetbookUseCase(
-                name: 'Usecase3',
-                builder: (BuildContext context) {
-                  return Scaffold();
-                },
-              ),
-            ],
-          ),
-        ],
-      )
-    ]);
+    final lightTheme =
+        WidgetbookTheme(name: 'Light theme', data: ThemeData.light());
+
+    final darkTheme =
+        WidgetbookTheme(name: 'Dark theme', data: ThemeData.dark());
+
+    return Widgetbook.material(
+      // addons: [
+      //   DeviceFrameAddon(
+      //     devices: [
+      //       Devices.ios.iPhone13Mini,
+      //       Devices.ios.iPhone13ProMax,
+      //     ],
+      //     initialDevice: Devices.ios.iPhone13ProMax,
+      //   ),
+      //   MaterialThemeAddon(
+      //       themes: [lightTheme, darkTheme], initialTheme: lightTheme)
+      // ],
+      directories: [
+        // WidgetbookFolder(
+        //   name: 'Atoms',
+        //   children: [
+        //     WidgetbookComponent(
+        //       name: 'Component1',
+        //       useCases: [
+        //         WidgetbookUseCase(
+        //           name: 'Usecase1',
+        //           builder: (BuildContext context) {
+        //             final width = context.knobs.double.slider(
+        //                 label: 'width', min: 10, max: 100, initialValue: 50);
+        //             final height = context.knobs.double.slider(
+        //                 label: 'height', min: 10, max: 100, initialValue: 50);
+
+        //             return Scaffold(
+        //               body: Center(
+        //                 child: Container(
+        //                   color: Colors.purpleAccent,
+        //                   width: width,
+        //                   height: height,
+        //                 ),
+        //               ),
+        //             );
+        //           },
+        //         ),
+        //         WidgetbookUseCase(
+        //           name: 'Usecase2',
+        //           builder: (BuildContext context) {
+        //             return Scaffold();
+        //           },
+        //         ),
+        //         WidgetbookUseCase(
+        //           name: 'Usecase3',
+        //           builder: (BuildContext context) {
+        //             return Scaffold();
+        //           },
+        //         ),
+        //       ],
+        //     ),
+        //     WidgetbookComponent(
+        //       name: 'Component2',
+        //       useCases: [
+        //         WidgetbookUseCase(
+        //           name: 'Usecase1',
+        //           builder: (BuildContext context) {
+        //             return Scaffold();
+        //           },
+        //         ),
+        //         WidgetbookUseCase(
+        //           name: 'Usecase2',
+        //           builder: (BuildContext context) {
+        //             return Scaffold();
+        //           },
+        //         ),
+        //         WidgetbookUseCase(
+        //           name: 'Usecase3',
+        //           builder: (BuildContext context) {
+        //             return Scaffold();
+        //           },
+        //         ),
+        //       ],
+        //     ),
+        //   ],
+        // )
+      ],
+    );
   }
 }
