c0f54457e34a2fef73414b8eff59805b09667e2a
Khanin <khanin.dev@gmail.com>
2024-04-07 11:59:13 +0700

    example done

--- wrap_example/lib/main.dart
+++ wrap_example/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,15 +26,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -40,17 +33,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 4;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
+  }
+
+  void _decrementCounter() {
+    if (_counter > 0) {
+      setState(() {
+        _counter--;
+      });
+    }
   }
 
   @override
@@ -60,25 +56,80 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text(
+            'Number of items',
+          ),
+          Text(
+            '$_counter',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [Text('FractionallySizedBox(widthFactor:0.25)')],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Wrap(
+            children: List.generate(
+              _counter,
+              (index) => const CustomFractionallySizedBox(widthFactor: 0.25),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [Text('FractionallySizedBox(widthFactor:0.3)')],
+            ),
+          ),
+          Wrap(
+            alignment: WrapAlignment.start,
+            children: List.generate(
+              _counter,
+              (index) => const CustomFractionallySizedBox(widthFactor: 0.3),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: SizedBox.expand(
+        child: Stack(
+          alignment: Alignment.bottomRight,
+          clipBehavior: Clip.none,
+          children: [
+            Positioned(
+              bottom: 72,
+              child: FloatingActionButton(
+                  onPressed: _incrementCounter, child: const Icon(Icons.add)),
+            ),
+            Positioned(
+              child: FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  child: const Icon(Icons.remove)),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    );
+  }
+}
+
+class CustomFractionallySizedBox extends StatelessWidget {
+  const CustomFractionallySizedBox({super.key, required this.widthFactor});
+
+  final double widthFactor;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: widthFactor,
+      child: Container(
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(),
+          ),
+          child: Text('$widthFactor')),
     );
   }
 }
