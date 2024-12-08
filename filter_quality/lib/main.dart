ee9137ec62f3944049b70e2db0d7907e11e6f93e
Khanin <khanin.dev@gmail.com>
2024-12-08 17:54:24 +0700

    filter quality example done

--- filter_quality/lib/main.dart
+++ filter_quality/lib/main.dart
@@ -33,6 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final List<double> containerSizeList = [50, 100, 200];
   final List<double> imageSizeList = [50, 100, 200, 300];
   final double gap = 8;
+  final double leftColumnWidth = 100;
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
               return Tab(
                 child: Column(
                   children: [
-                    Text('Container Size'),
+                    const Text('Container Size'),
                     Text('$size x $size'),
                   ],
                 ),
@@ -73,93 +74,112 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
               TabBarView(
+                physics: const NeverScrollableScrollPhysics(),
                 children: [
                   ...List.generate(containerSizeList.length, (z) {
                     final containerSize = containerSizeList[z];
                     return Padding(
-                      padding: const EdgeInsets.all(16.0),
+                      padding: const EdgeInsets.all(20.0),
                       child: Center(
                         child: SingleChildScrollView(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(
-                                      'Container Size $containerSize x $containerSize pixel'),
-                                  const SizedBox(width: 20),
-                                  Container(
-                                    alignment: Alignment.center,
-                                    width: containerSize,
-                                    height: containerSize,
-                                    color: Colors.red,
-                                  )
-                                ],
-                              ),
-                              const SizedBox(
-                                height: 50,
-                              ),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  const SizedBox(width: 200),
-                                  Column(
-                                    children: [
-                                      const Text('Source image size'),
-                                      const SizedBox(height: 8),
-                                      Row(
-                                        children: [
-                                          ...List.generate(
-                                            imageSizeList.length,
-                                            (x) {
-                                              final size = imageSizeList[x];
-                                              return Padding(
-                                                padding:
-                                                    EdgeInsets.only(right: gap),
-                                                child: SizedBox(
-                                                  width: containerSize,
-                                                  child: Text('$size x $size'),
-                                                ),
-                                              );
-                                            },
-                                          ),
-                                        ],
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
-                              ...List.generate(FilterQuality.values.length,
-                                  (y) {
-                                return Padding(
-                                  padding: const EdgeInsets.only(bottom: 8.0),
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                        'Container Size $containerSize x $containerSize pixel'),
+                                    const SizedBox(width: 20),
+                                    Container(
+                                      alignment: Alignment.center,
+                                      width: containerSize,
+                                      height: containerSize,
+                                      color: Colors.red,
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 50,
+                                ),
+                                IntrinsicHeight(
                                   child: Row(
                                     mainAxisSize: MainAxisSize.min,
                                     children: [
-                                      SizedBox(
-                                          width: 200,
-                                          child: Text(FilterQuality.values[y]
-                                              .toString())),
-                                      ...List.generate(imageSizeList.length,
-                                          (x) {
-                                        return Padding(
-                                          padding: EdgeInsets.only(right: gap),
-                                          child: Image.asset(
-                                            'assets/flutter_logo_${imageSizeList[x]}px.png',
-                                            fit: BoxFit.cover,
-                                            width: containerSize,
-                                            height: containerSize,
-                                            filterQuality:
-                                                FilterQuality.values[y],
-                                          ),
-                                        );
-                                      })
+                                      Container(
+                                        width: leftColumnWidth,
+                                        alignment: Alignment.topLeft,
+                                        child: Text('FilterQuality'),
+                                      ),
+                                      Column(
+                                        children: [
+                                          const Text('Source image size'),
+                                          const SizedBox(height: 8),
+                                          Row(
+                                            children: [
+                                              ...List.generate(
+                                                imageSizeList.length,
+                                                (x) {
+                                                  final size = imageSizeList[x];
+                                                  return Padding(
+                                                    padding: EdgeInsets.only(
+                                                        right: gap),
+                                                    child: SizedBox(
+                                                      width: containerSize,
+                                                      child:
+                                                          Text('$size x $size'),
+                                                    ),
+                                                  );
+                                                },
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      )
                                     ],
                                   ),
-                                );
-                              })
-                            ],
+                                ),
+                                ...List.generate(FilterQuality.values.length,
+                                    (y) {
+                                  return Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0),
+                                    child: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        SizedBox(
+                                          width: leftColumnWidth,
+                                          child: Text(
+                                            FilterQuality.values[y].name,
+                                          ),
+                                        ),
+                                        ...List.generate(
+                                          imageSizeList.length,
+                                          (x) {
+                                            final int sizeInt =
+                                                imageSizeList[x].toInt();
+
+                                            return Padding(
+                                              padding:
+                                                  EdgeInsets.only(right: gap),
+                                              child: Image.asset(
+                                                'assets/flutter_logo_${sizeInt}px.png',
+                                                fit: BoxFit.cover,
+                                                width: containerSize,
+                                                height: containerSize,
+                                                filterQuality:
+                                                    FilterQuality.values[y],
+                                              ),
+                                            );
+                                          },
+                                        )
+                                      ],
+                                    ),
+                                  );
+                                })
+                              ],
+                            ),
                           ),
                         ),
                       ),
