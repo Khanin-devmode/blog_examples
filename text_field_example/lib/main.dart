2826a97c14a038b270385962659fd4e53d7d965b
Khanin <khanin.dev@gmail.com>
2025-02-09 21:11:03 +0700

    wip

--- text_field_example/lib/main.dart
+++ text_field_example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TextFormField Example',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -31,81 +31,100 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final outlineInputBorder = OutlineInputBorder(
-      borderRadius: BorderRadius.circular(1),
-      borderSide: const BorderSide(width: 1.2));
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              children: [
+                CustomContainer(
+                  size: 48,
+                ),
+                Expanded(
+                  child: CustomTextFormField(),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                CustomContainer(
+                  size: 48,
+                ),
+                Expanded(
+                  child: IncorrectTextFormField(),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextFormField extends StatelessWidget {
+  final double? fontSize;
+  final double? height;
+  final EdgeInsetsGeometry? contentPadding;
+
+  const CustomTextFormField({
+    super.key,
+    this.fontSize,
+    this.height,
+    this.contentPadding,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      //   title: Text(widget.title),
-      // ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // const Text(
-              //   'Max Lines 5',
-              // ),
-              // TextFormField(
-              //   decoration: InputDecoration(border: outlineInputBorder),
-              //   maxLines: 4,
-              // ),
-              // const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(border: outlineInputBorder),
-                minLines: 2,
-                maxLines: 2,
-              ),
-              // const SizedBox(height: 16.0),
-              // TextFormField(
-              //   decoration: InputDecoration(border: outlineInputBorder),
-              // ),
-              // const SizedBox(height: 16.0),
-              // Row(
-              //   children: [
-              //     Expanded(
-              //         child: Container(
-              //       height: 100,
-              //       color: Colors.amber,
-              //     )),
-              //     Expanded(
-              //       child: TextFormField(
-              //         style: TextStyle(fontSize: 60, height: 1),
-              //         decoration: InputDecoration(
-              //             border: outlineInputBorder,
-              //             contentPadding: const EdgeInsets.all(20)),
-              //       ),
-              //     )
-              //   ],
-              // ),
-              // const SizedBox(height: 16),
-              // Row(
-              //   children: [
-              //     Expanded(
-              //         child: Container(
-              //       height: 100,
-              //       color: Colors.amber,
-              //     )),
-              //     Expanded(
-              //       child: TextFormField(
-              //         style: TextStyle(fontSize: 20, height: 1),
-              //         maxLines: 3,
-              //         decoration: InputDecoration(
-              //           border: outlineInputBorder,
-              //           contentPadding: const EdgeInsets.all(20),
-              //         ),
-              //       ),
-              //     ),
-              //   ],
-              // )
-            ],
-          ),
-        ),
+    return TextFormField(
+      style: TextStyle(fontSize: fontSize, height: height),
+      decoration: InputDecoration(
+          border: const OutlineInputBorder(), contentPadding: contentPadding),
+    );
+  }
+}
+
+class IncorrectTextFormField extends StatelessWidget {
+  const IncorrectTextFormField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      decoration: BoxDecoration(border: Border.all()),
+      child: TextFormField(
+        style: const TextStyle(fontSize: 32, height: 1.0),
+        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 20)),
+      ),
+    );
+  }
+}
+
+class CustomContainer extends StatelessWidget {
+  final int size;
+  final Color color;
+
+  const CustomContainer({
+    super.key,
+    this.size = 48,
+    this.color = Colors.red,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      width: size.toDouble(),
+      height: size.toDouble(),
+      alignment: Alignment.center,
+      child: Text(
+        '$size x $size',
+        style: const TextStyle(color: Colors.white, fontSize: 12),
       ),
     );
   }
