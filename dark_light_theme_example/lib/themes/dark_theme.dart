8b812ff9296ac62735de00b53117dff6d441c231
Khanin <khanin.dev@gmail.com>
2025-03-30 20:31:04 +0700

    create light and dark theme colors

---
+++ dark_light_theme_example/lib/themes/dark_theme.dart
@@ -0,0 +1,14 @@
+import 'package:dark_light_theme_example/themes/design_system_colors.dart';
+import 'package:flutter/material.dart';
+
+const lightTheme = DesignSystemColors(
+  background: Color(0xFF1E1E1E),
+  border: Color(0xFF424242),
+  shadow: Color(0x80000000),
+  title: Color(0xFFFFFFFF),
+  subTitle: Color(0xFFBDBDBD),
+  content: Color(0xFFE0E0E0),
+  icon: Color(0xFFBB86FC),
+  actionButtonBackground: Color(0xFF2C2C2C),
+  actionButtonText: Color(0xFFBB86FC),
+);
---
+++ dark_light_theme_example/lib/themes/light_theme.dart
@@ -0,0 +1,14 @@
+import 'package:dark_light_theme_example/themes/design_system_colors.dart';
+import 'package:flutter/material.dart';
+
+const lightTheme = DesignSystemColors(
+  background: Color(0xFFFFFFFF),
+  border: Color(0xFFE0E0E0),
+  shadow: Color(0x40000000),
+  title: Color(0xFF000000),
+  subTitle: Color(0xFF616161),
+  content: Color(0xFF212121),
+  icon: Color(0xFF6200EE),
+  actionButtonBackground: Color(0xFFF5F5F5),
+  actionButtonText: Color(0xFF6200EE),
+);
