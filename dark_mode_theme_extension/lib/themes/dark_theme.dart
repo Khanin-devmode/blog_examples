505cde0383f87d2bf6c47dc110b01c3f945bd9d0
Khanin <khanin.dev@gmail.com>
2025-03-30 20:39:47 +0700

    apply light and dark theme in material app

--- dark_mode_theme_extension/lib/themes/dark_theme.dart
+++ dark_mode_theme_extension/lib/themes/dark_theme.dart
@@ -1,7 +1,7 @@
-import 'package:dark_light_theme_example/themes/design_system_colors.dart';
+import 'package:dark_mode_theme_extension/themes/design_system_colors.dart';
 import 'package:flutter/material.dart';
 
-const lightTheme = DesignSystemColors(
+const darkTheme = DesignSystemColors(
   background: Color(0xFF1E1E1E),
   border: Color(0xFF424242),
   shadow: Color(0x80000000),
